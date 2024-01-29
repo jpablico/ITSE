e0b6f2a5749f4e53d85ec9c7aea892fd1b869625
Josh Pablico <jbpablico27@gmail.com>
2024-01-29 10:33:47 -0600

    JS cmp.

---
+++ 2302/Practice1/script.js
@@ -0,0 +1,34 @@
+let userName = 'Bob';
+let userAge = 26;
+const userCode = '34';
+
+/* 
+3.
+	a - userName and userAge;
+	b - userCode;
+	c - userName and userCode;
+	d - userAge;
+*/
+
+/*
+5. 
+	Yes, the name changed to "Sue".
+	Yes, the value changed to 35.
+	No, the value did not change.
+*/
+
+let total = 5 + 10;
+let x = 5;
+let y = 10;
+total = x + y;
+
+let message = 'Sue, Welcome to our site';
+
+let greeting = 'Welcome to our site';
+
+message = userName + ", " +  greeting;
+// 6. Yes, it matches the original.
+
+let width = 5;
+width++;
+//7. The currentl value is 6.
