ebc3f4c3e9b612b02e841a21761f3d151bf4a3f9
Josh Pablico <jbpablico27@gmail.com>
2025-01-31 01:05:20 -0600

    Abandoning. Going to reuse old projects for assignment.

---
+++ 2371/Week-2/LabOne/my-react-app/src/scripts/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Header from './components/Header.jsx';
+
+import '../styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ 2371/Week-2/LabOne/my-react-app/src/scripts/components/Header.jsx
@@ -0,0 +1,21 @@
+
+
+function Header() {
+  return (
+    <header className="header">
+      <ul className="header__nav">
+        <li className="header__nav__item">
+          <a href="#" className="header__nav__link">Home</a>
+        </li>
+        <li className="header__nav__item">
+          <a href="#" className="header__nav__link">About</a>
+        </li>
+        <li className="header__nav__item">
+          <a href="#" className="header__nav__link">Services</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
