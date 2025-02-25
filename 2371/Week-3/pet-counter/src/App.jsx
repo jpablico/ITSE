bb54f9338bba909ba23dbd8d44db6bd1418de171
Josh Pablico <jbpablico27@gmail.com>
2025-02-25 00:50:53 -0600

    Finished.

---
+++ 2371/Week-3/pet-counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    const [dogCount, setDogCount] = useState(0);
+    const [catCount, setCatCount] = useState(0);
+    const [rabbitCount, setRabbitCount] = useState(0);
+
+    const handleClick = (pet) => {
+        if (pet === "dog") setDogCount(dogCount + 1);
+        else if (pet === "cat") setCatCount(catCount + 1);
+        else if (pet === "rabbit") setRabbitCount(rabbitCount + 1);
+    };
+
+    return (
+        <div className="container">
+            <header>
+                <h1>🐶🐱🐰</h1>
+                <h1>Pet Rescue Counter</h1>
+            </header>
+            <div className="counter-container">
+                <button onClick={() => handleClick("dog")}>Dogs: {dogCount}</button>
+                <button onClick={() => handleClick("cat")}>Cats: {catCount}</button>
+                <button onClick={() => handleClick("rabbit")}>Rabbits: {rabbitCount}</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
